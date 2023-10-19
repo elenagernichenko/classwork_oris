bbc246e67c0c1e2f901435f07a4a6ad891311ee0
elenagernichenko <elenagernichenko@yandex.ru>
2023-10-19 22:15:45 +0300

    added 5th homework (sessions)

--- webapp/src/main/java/servlets/AuthServlet.java
+++ webapp/src/main/java/servlets/AuthServlet.java
@@ -8,10 +8,7 @@ import repositories.UsersRepositoryJdbcImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.sql.*;
 import java.util.Optional;
@@ -44,20 +41,6 @@ public class AuthServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("session_uuid")) {
-                    String uuidString = cookie.getValue();
-                    UUID uuid = UUID.fromString(uuidString);
-
-                    if (cookiesRepository.findSession(uuid)) {
-                        response.sendRedirect("users");
-                        return;
-                    }
-                }
-            }
-        }
         request.getRequestDispatcher("/html/auth.html").forward(request, response);
     }
 
@@ -75,11 +58,8 @@ public class AuthServlet extends HttpServlet {
         String status;
 
         if (user.isPresent()) {
-            UUID uuid = UUID.randomUUID();
-            Cookie cookie = new Cookie("session_uuid", uuid.toString());
-            response.addCookie(cookie);
-
-            cookiesRepository.saveSession(uuid, user.get().getId());
+            HttpSession httpSession = request.getSession(true);
+            httpSession.setAttribute("authenticated", true);
 
             result = username + ", you have successfully logged in to your account";
             status = "login in successfully";
